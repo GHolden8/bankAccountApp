52d45aabccab2d0ab7fdf8b375859d7259671862
GHolden8 <117676816+GHolden8@users.noreply.github.com>
2023-11-20 22:20:30 -0800

    Update Customer.java and brought code from wrong branch

--- Customer.java
+++ Customer.java
@@ -1,7 +1,15 @@
 package Final;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
 public class Customer extends User {
 	
+	public static Connection connection;
+	
+	public String name;
 	public int checking;
 	public int savings;
 	
@@ -11,8 +19,60 @@ public class Customer extends User {
 		this.savings = savings;
 	}
 	
-	public int getChecking() {
-		return this.checking;
+	public static void connection() {
+		
+		String url = "jdbc:mysql://localhost:3306/finalproject";
+		String username = "root";
+		String password = "Redvader09!";
+
+		try {
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Connected");
+		}catch(Exception e) {
+			System.out.println("Could not connect");
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+	
+	public static void add(String firstName, String lastName, String username, String password) {
+		try {
+			Statement statement = connection.createStatement();
+			System.out.println("insert into bank_accounts (firstName, lastName, username, password, checking, savings) "
+					+ "values ( '" + firstName + "', '" + lastName + "', '" + username + "', '" + password + "', " + 0 + ", " + 0 + ")");
+			statement.executeUpdate("insert into bank_accounts (firstName, lastName, username, password, checking, savings) "
+					+ "values ( '" + firstName + "', '" + lastName + "', '" + username + "', '" + password + "', " + 0 + ", " + 0 + ")");
+			System.out.println("User Created");
+			
+		}catch(Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}
+	
+	public static void setChecking(String username, int amount) {
+		
+		try {
+			Statement statement = connection.createStatement();	
+			System.out.println("update bank_accounts set checking = " + amount + " where username = \"" + username + "\"");
+			statement.executeUpdate("update bank_accounts set checking = " + amount + " where username = \"" + username + "\"");
+
+		}catch(Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}	
+
+	public static void setSavings(String username, int amount) {
+	
+		try {
+			Statement statement = connection.createStatement();		
+			System.out.println("update bank_accounts set savings = " + amount + " where username = \"" + username + "\"");
+			statement.executeUpdate("update bank_accounts set savings = " + amount + " where username = \"" + username + "\"");
+
+		}catch(Exception e) {
+			System.out.println("Exception " + e.getMessage());
+		}
+	}	
+	
+	public int getChecking(String username) {
 		
 		try {
 			Statement statement = connection.createStatement();
@@ -20,16 +80,17 @@ public class Customer extends User {
 			System.out.println("select checking from bank_accounts where username = \"" + username + "\"");
 
 			while(result.next()) {
-				return result;
+				return result.getInt("checking");
 			}
 
 		}catch(Exception e) {
 			System.out.println("Exception " + e.getMessage());
 		}
+		
+		return 0;
 	}
 	
-	public int getSavings() {
-		return this.savings;
+	public int getSavings(String username) {
 		
 		try {
 			Statement statement = connection.createStatement();
@@ -37,12 +98,14 @@ public class Customer extends User {
 			System.out.println("select savings from bank_accounts where username = \"" + username + "\"");
 
 			while(result.next()) {
-				return result;
+				return result.getInt("savings");
 			}
 
 		}catch(Exception e) {
 			System.out.println("Exception " + e.getMessage());
 		}
+		
+		return 0;
 	}
 	
 	
